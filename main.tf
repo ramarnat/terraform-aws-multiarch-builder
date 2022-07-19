c7a99eab7572695a1203dc048f5e3fc446c675f2
Job Céspedes Ortiz <jobcespedes@gmail.com>
2022-07-19 11:10:38 -0600

    feat: add local-exec trigger

--- main.tf
+++ main.tf
@@ -208,6 +208,11 @@ resource "null_resource" "client_config" {
 
   count = var.handle_client_config && var.create_amd64 && var.create_arm64 ? 1 : 0
 
+
+  triggers {
+    config_file_exists = fileexists(pathexpand("~/.docker/buildx/instances/multiarch-builder"))
+  }
+
   provisioner "local-exec" {
     command     = local.client_config_arm64_cmd
     interpreter = ["/bin/bash", "-c"]
